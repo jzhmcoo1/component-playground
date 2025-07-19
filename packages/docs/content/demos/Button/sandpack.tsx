5f7978104206a260846a98979f54ab2f8e86425b
Longsi <jzhmzlxcoo1@gmail.com>
2025-07-20 00:33:14 +0800

    feat: init

--- packages/docs/content/demos/Button/sandpack.tsx
+++ packages/docs/content/demos/Button/sandpack.tsx
@@ -1,4 +1,9 @@
-import { Sandpack } from "@codesandbox/sandpack-react";
+import {
+  SandpackLayout,
+  SandpackProvider,
+  SandpackPreview,
+  SandpackCodeEditor,
+} from "@codesandbox/sandpack-react";
 import fs from "fs";
 import path from "path";
 
@@ -13,7 +18,7 @@ export default async function ButtonDemoSandpack() {
   );
 
   return (
-    <Sandpack
+    <SandpackProvider
       template="react"
       options={{
         externalResources: [
@@ -37,6 +42,38 @@ export default async function ButtonDemoSandpack() {
           code: nxUiIndexJs,
         },
       }}
-    />
+    >
+      <SandpackLayout>
+        <SandpackCodeEditor showLineNumbers={true} />
+        <SandpackPreview showOpenInCodeSandbox={false} />
+      </SandpackLayout>
+    </SandpackProvider>
+    // <Sandpack
+    //   template="react"
+    //   options={{
+    //     externalResources: [
+    //       "https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4",
+    //     ],
+    //     showRefreshButton: true,
+    //     showLineNumbers: true,
+    //   }}
+    //   files={{
+    //     "/App.js": {
+    //       code: fileContent,
+    //       active: true,
+    //     },
+    //     "/node_modules/nx-ui/package.json": {
+    //       hidden: true,
+    //       code: JSON.stringify({
+    //         name: "@nx-ui",
+    //         main: "./index.js",
+    //       }),
+    //     },
+    //     "/node_modules/nx-ui/index.js": {
+    //       hidden: true,
+    //       code: nxUiIndexJs,
+    //     },
+    //   }}
+    // />
   );
 }
